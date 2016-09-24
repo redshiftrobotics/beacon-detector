91a432b80ead3f191b71292f0d1e25aebb71cfeb
Ari Porad <ari@ariporad.com>
2016-09-24 08:56:43 -0700

    feat: show beacon state

--- index.py
+++ index.py
@@ -5,8 +5,10 @@ from PIL import Image
 
 DOMINANT_THRESHOLD = 210
 OTHER_THRESHOLD = 170
-GREEN_THRESHOLD = 200
-THUMBNAIL_SIZE = 500
+GREEN_THRESHOLD = 150 #200
+THUMBNAIL_SIZE = 750
+BEACON_VIEW_WIDTH = 500
+BEACON_VIEW_HEIGHT = 250
 
 CLASSIFICATION_THRESHOLD = 1000
 CLASSIFICATION_OTHER_THRESHOLD = 300
@@ -53,8 +55,23 @@ def process_image(image):
 
   im.paste(thumbnail, (im.size[0] - thumbnail.size[0], im.size[1] - thumbnail.size[1]))
 
-  im.show('test')
   actual_state = _classify_image(num_red, num_blue)
+  if actual_state == 'red' or actual_state == 'blue':
+    color = (255, 0, 0) if actual_state == 'red' else (0, 0, 255)
+    color_im = Image.new('RGB', (BEACON_VIEW_WIDTH, BEACON_VIEW_HEIGHT), color=color)
+    im.paste(color_im, (0, im.size[1] - BEACON_VIEW_HEIGHT))
+  elif actual_state == 'bluered' or actual_state =='redblue':
+    color_1 = (0, 0, 255)
+    color_2 = (255, 0, 0)
+    if actual_state == 'redblue':
+      color_1, color_2 = color_2, color_1
+    color_1_im = Image.new('RGB', (BEACON_VIEW_WIDTH // 2, BEACON_VIEW_HEIGHT), color=color_1)
+    color_2_im = Image.new('RGB', (BEACON_VIEW_WIDTH // 2, BEACON_VIEW_HEIGHT), color=color_2)
+    im.paste(color_1_im, (0, im.size[1] - BEACON_VIEW_HEIGHT))
+    im.paste(color_2_im, (BEACON_VIEW_WIDTH // 2, im.size[1] - BEACON_VIEW_HEIGHT))
+
+  im.show('test')
+
   print('{}: Red: {}, Blue: {}, Expected: {}, Actual: {}, OK: {}'.format(name, num_red, num_blue, state, actual_state, state == actual_state))
   if state == actual_state:
     num_pass = num_pass + 1
@@ -65,13 +82,15 @@ def process_image(image):
 
 images = []
 
-for state in ['blue', 'red', 'bluered']: # ['blue']: #['bluered', 'red', 'blue']:
+for state in ['blue', 'red', 'bluered']: #['blue', 'red', 'bluered']: # ['blue']: #['bluered', 'red', 'blue']:
   for image in listdir('images/{}'.format(state)):
     if image[-4:].lower() == '.jpg':
       images.append((state, 'images/{}/{}'.format(state, image)))
 
 
 shuffle(images)
+
+#images = [('blue', 'images/blue/0.jpg')]
 
 for image in images:
   process_image(image)
