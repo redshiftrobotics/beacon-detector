70d0b79cbd6c09e80e1f0b9f5c8797503543ecbe
Ari Porad <ari@ariporad.com>
2016-09-24 13:46:49 -0700

    refactor: split up the huge functions

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from os import listdir
 from time import sleep
 from random import shuffle
+from functools import reduce
 
 from PIL import Image, ImageFont, ImageDraw
 
@@ -31,57 +32,42 @@ num_fail = 0
 
 font = ImageFont.truetype('font.ttf', FONT_SIZE)
 
-def _classify_image(red, blue):
-  if blue <= CLASSIFICATION_OTHER_THRESHOLD and red > CLASSIFICATION_THRESHOLD:
+def _classify_image(reds, blues):
+  num_red = reduce(lambda x, num: num + x, reds)
+  num_blue = reduce(lambda x, num: num + x, blues)
+  if num_blue <= CLASSIFICATION_OTHER_THRESHOLD and num_red > CLASSIFICATION_THRESHOLD:
     return 'red'
-  elif red <= CLASSIFICATION_OTHER_THRESHOLD and blue > CLASSIFICATION_THRESHOLD:
+  elif num_red <= CLASSIFICATION_OTHER_THRESHOLD and num_blue > CLASSIFICATION_THRESHOLD:
     return 'blue'
-  elif red < CLASSIFICATION_THRESHOLD and blue < CLASSIFICATION_THRESHOLD:
+  elif num_red < CLASSIFICATION_THRESHOLD and num_blue < CLASSIFICATION_THRESHOLD:
     return None
   else:
     return 'bluered'
 
-def process_image(image):
-  global num_pass, num_fail
-  state, name = image
+
+def _get_image(name):
   im = Image.open(name)
   thumbnail = im.copy()
   thumbnail.thumbnail((THUMBNAIL_SIZE, THUMBNAIL_SIZE))
   im.thumbnail((IMAGE_SIZE, IMAGE_SIZE))
 
-  draw = ImageDraw.Draw(im)
-
   pix = im.load()
 
-  num_red = 0
-  num_blue = 0
+  return im, thumbnail, pix
 
-  def _process_pixel(pxl):
-    nonlocal num_red, num_blue
-    R, G, B = pxl
-    if R < BLU_R and G < BLU_G and B > BLU_B:
-      num_blue = num_blue + 1
-      return (0, 0, 255)
-    if R > RED_R and G < RED_G and B < RED_B:
-      num_red = num_red + 1
-      return (255, 0, 0)
-    return (0, 0, 0)
 
-  for i in range(im.size[0]):
-    for j in range(im.size[1]):
-      pix[i, j] = _process_pixel(pix[i, j])
-
+def _add_helper_views(name, im, state, thumbnail):
+  draw = ImageDraw.Draw(im)
   im.paste(thumbnail, (im.size[0] - thumbnail.size[0], im.size[1] - thumbnail.size[1]))
 
-  actual_state = _classify_image(num_red, num_blue)
-  if actual_state == 'red' or actual_state == 'blue':
-    color = (255, 0, 0) if actual_state == 'red' else (0, 0, 255)
+  if state == 'red' or state == 'blue':
+    color = (255, 0, 0) if state == 'red' else (0, 0, 255)
     color_im = Image.new('RGB', (BEACON_VIEW_WIDTH, BEACON_VIEW_HEIGHT), color=color)
     im.paste(color_im, (0, im.size[1] - BEACON_VIEW_HEIGHT))
-  elif actual_state == 'bluered' or actual_state =='redblue':
+  elif state == 'bluered' or state =='redblue':
     color_1 = (0, 0, 255)
     color_2 = (255, 0, 0)
-    if actual_state == 'redblue':
+    if state == 'redblue':
       color_1, color_2 = color_2, color_1
     color_1_im = Image.new('RGB', (BEACON_VIEW_WIDTH // 2, BEACON_VIEW_HEIGHT), color=color_1)
     color_2_im = Image.new('RGB', (BEACON_VIEW_WIDTH // 2, BEACON_VIEW_HEIGHT), color=color_2)
@@ -90,15 +76,41 @@ def process_image(image):
 
   draw.text((10, 10), name.lstrip('images/'), font=font)
 
-  im.show('test')
+
+def process_image(image):
+  global num_pass, num_fail
+  state, name = image
+  im, thumbnail, pix = _get_image(name)
+
+  reds =  [0] * im.size[0]
+  blues = [0] * im.size[0]
+
+  def _process_pixel(x, y, pxl):
+    R, G, B = pxl
+    if R < BLU_R and G < BLU_G and B > BLU_B:
+      blues[x] += 1
+      return (0, 0, 255)
+    if R > RED_R and G < RED_G and B < RED_B:
+      reds[x] += 1
+      return (255, 0, 0)
+    return (0, 0, 0)
+
+  for x in range(im.size[0]):
+    for y in range(im.size[1]):
+      pix[x, y] = _process_pixel(x, y, pix[x, y])
+
+
+  actual_state = _classify_image(reds, blues)
+  _add_helper_views(name, im, actual_state, thumbnail)
+
+  im.show()
 
   print('{}: Red: {}, Blue: {}, Expected: {}, Actual: {}, OK: {}' \
-        .format(name, num_red, num_blue, state, actual_state, state == actual_state))
+        .format(name, sum(reds), sum(blues), state, actual_state, state == actual_state))
   if state == actual_state:
     num_pass = num_pass + 1
   else:
     num_fail = num_fail + 1
-    im.show()
 
 
 images = []
